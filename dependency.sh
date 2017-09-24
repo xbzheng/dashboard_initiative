1a664738c2932dd3a18025460ee66cae04debcf0
Ivan Zheng <ivanzheng@Ivans-MacBook-Air.local>
2017-09-23 23:52:18 -0400

    dash started

---
+++ dependency.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+pip install dash==0.18.3  # The core dash backend
+pip install dash-renderer==0.10.0  # The dash front-end
+pip install dash-html-components==0.7.0  # HTML components
+pip install dash-core-components==0.12.6  # Supercharged components
+pip install plotly --upgrade  # Latest Plotly graphing library
